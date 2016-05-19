7940baf3543f974a5514b41889a8e89cfc95fc87
guoxuemeng <89016230@qq.com>
2016-05-19 16:08:54 +0800

    update

--- app/Http/Controllers/Backend/AuthController.php
+++ app/Http/Controllers/Backend/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
 
     public function postLogin(Request $request)
     {
-        $result = ['flag' => false, 'msg' => '登陆失败'];
+        $result = ['flag' => false, 'msg' => '登陆失败，账户和密码不企配'];
         $credentials = $request->only('email', 'password');
         if (Auth::guard('admin')->attempt($credentials)) {
             $token = uniqid();
